3d6f9455f8139651d87c4eb0f05b9acee55fa1ae
Bugra Aydingoz <bugraaydingoz@gmail.com>
2019-03-24 12:20:08 +0100

    Add font & linux commands

--- install.sh
+++ install.sh
@@ -17,15 +17,36 @@ if [ "$(uname)" == "Darwin" ]; then
   # Change default shell to fish
   chsh -s /usr/local/bin/fish
   curl -L https://get.oh-my.fish | fish
-
-  echo "Add osx/Dracula.terminal to your terminal theme."
-  echo "Install Roboto Mono"
+  omf install robbyrussell
 
   # Symlink .vimrc files
   stow vim
 
+  # TODO: Automate
+  echo "Install fonts/RobotoMono-Regular.ttf"
+  echo "Add osx/Dracula.terminal to your terminal theme."
+
 elif [ "$(expr substr $uname -s) 1 5" ] == "Linux" ]; then
 
-  echo "Not implemented."
+  # Upgrade the system
+  sudo -v
+  sudo apt update && sudo apt upgrade -y 
+  sudo add-apt-repository ppa:jonathonf/vim
+
+  # Install packages
+  sudo apt install -y git
+  sudo apt install -y fish
+  sudo apt install -y vim
+  sudo apt install -y tmux
+  sudo apt install -y stow
+
+  # Change default shell to fish
+  chsh -s /usr/local/bin/fish
+  curl -L https://get.oh-my.fish | fish
+  omf install robbyrussell
+
+  # Symlink .vimrc files
+  stow vim
 
 fi
+
